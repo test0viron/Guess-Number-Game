28b1d448f7546e748f3996eccf78d73c7d4650c2
PC <tau013@gmail.com>
2026-09-07 19:32:59 +0200

    main.py - implementation of rankings.py module to main program

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import gng_game, inputs, rules
+import gng_game, inputs, rules, rankings
 
 nickname = inputs.get_nickname()
 print("")
@@ -6,7 +6,7 @@ print("")
 print(f'!!! WELCOME TO GUESS NUMBER GAME {nickname}!!!')
 print("")
 
-print("To start a game please select difficulty level.")
+print("To start a game please select difficulty level by its number.")
 print("1. Easy\n2. Medium\n3. Hard")
 print("")
 
@@ -31,5 +31,15 @@ if end_result == 0:
     print(f'Better luck next time {nickname}!')
 else:
     print("=== GAME OVER ===")
-    print(f'Congratulations, {nickname}! You score: {end_result} points')
+    print("")
+    print(f'Congratulations, {nickname}! Your score: {end_result} {"point" if end_result == 1 else "points"}')
+
+ranking = rankings.rankings(nickname, end_result)
+print("")
+
+print("Your place in ranking:")
+
+for rank in ranking:
+
+    print(f'{rank["Position"]}. {rank["Nickname"]} - {rank["Result"]} {"point" if rank["Result"] == 1 else "points"}')
 
