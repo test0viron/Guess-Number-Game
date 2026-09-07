2bd5a579d6161326245d7bfd1c7611941c8785f0
PC <tau013@gmail.com>
2026-09-07 15:35:39 +0200

    rules.py - number range change for level hard gng game

--- rules.py
+++ rules.py
@@ -18,7 +18,7 @@ def rules(difficulty):
 
     elif difficulty == "Hard":
         print("Hard level game rules:")
-        print("1. You will need to chose one number form 1 to 20 range")
+        print("1. You will need to chose one number form 1 to 15 range")
         print("2. For every guess your attempt counter will rise by 1")
         print("3. Every correct guess in Medium level gives you 3 points in ranking")
         print("4. Every wrong answer just wastes one attempt")
